b5a937c0cbc1d7a6378ae7e0e1e490cd283c8f66
우진 <woojin9812071207@gmail.com>
2023-01-02 18:14:54 +0900

    login test

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -8,24 +8,46 @@ const LoginPage = () => {
     const [password, setPassword] = useState(""); 
     const [name, setName] = useState(""); 
     const [nickname, setNickname] = useState(""); 
-    const userObject = {
-        'email' : email,
-        'password' : password,
-        'name' : name,
-        'nickname' : nickname,
-    }
+    const [status, setStatus] = useState("");
+
 
     const registerHandler = () => {
+        const userObject = {
+            'email' : email,
+            'password' : password,
+            'name' : name,
+            'nickname' : nickname,
+        };
         console.log(userObject);
         const config = {"Content-Type" : 'application/json'};
         axios.post('/api/members/register', userObject, config)
         .then((res) => {
             console.log(res.data);
+            setStatus("가입 성공");
         })
         .catch((err) => {
             console.log(err);
+            setStatus("가입 실패");
         });
     };
+
+    const loginHandler = () => {
+        const userObject = {
+            'email' : email,
+            'password' : password,
+        };
+        const config = {"Content-Type" : 'application/json'};
+        axios.post('/api/members/login', userObject, config)
+        .then((res) => {
+            console.log(res.data);
+            setStatus("로그인 성공");
+        })
+        .catch((err) => {
+            console.log(err);
+            setStatus("로그인 실패");
+        });
+    };
+
     return (
         <div className="App">
             <div>
@@ -41,7 +63,10 @@ const LoginPage = () => {
                 <label>Nickname</label>
                 <input onChange={(e) => setNickname(e.target.value)}/>
                 <br/>
+                
                 <button onClick={() => registerHandler()}>가입</button>
+                <button onClick={() => loginHandler()}>로그인</button>
+                <br/>status : {status}
             </div>
         </div>
     )
