c770ceff601c07ab4e065a8520deeab2d37f8f35
우진 <woojin9812071207@gmail.com>
2023-01-02 19:29:58 +0900

    commit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,29 +3,67 @@ import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 
 const LoginPage = () => {
-    console.log("hi1");
     const [email, setEmail] = useState(""); 
     const [password, setPassword] = useState(""); 
     const [name, setName] = useState(""); 
     const [nickname, setNickname] = useState(""); 
-    const userObject = {
-        'email' : email,
-        'password' : password,
-        'name' : name,
-        'nickname' : nickname,
-    }
+    const [status, setStatus] = useState("");
+    const [text, setText] = useState("");
+
 
     const registerHandler = () => {
+        const userObject = {
+            'email' : email,
+            'password' : password,
+            'name' : name,
+            'nickname' : nickname,
+        };
         console.log(userObject);
         const config = {"Content-Type" : 'application/json'};
         axios.post('/api/members/register', userObject, config)
         .then((res) => {
             console.log(res.data);
+            setStatus("가입 성공");
+        })
+        .catch((err) => {
+            console.log(err);
+            setStatus("가입 실패");
+        });
+    };
+
+    const loginHandler = () => {
+        const userObject = {
+            'email' : email,
+            'password' : password,
+        };
+        const config = {"Content-Type" : 'application/json'};
+        axios.post('/api/members/login', userObject, config)
+        .then((res) => {
+            localStorage.setItem('accessToken', res.data);
+            axios.defaults.headers.common['x-access-token'] = res.data;
+            console.log(res.data);
+            setStatus("로그인 성공");
+        })
+        .catch((err) => {
+            console.log(err);
+            setStatus("로그인 실패");
+        });
+    };
+
+    const testfunc = () => {
+        let acctoken = localStorage.getItem('accessToken');
+        axios.get('/api/members/info', {headers : 
+            {token: acctoken}})
+        .then((res) => {
+            console.log(res.data);
+            setText(res.data);
         })
         .catch((err) => {
             console.log(err);
         });
-    };
+    }
+
+
     return (
         <div className="App">
             <div>
@@ -41,7 +79,12 @@ const LoginPage = () => {
                 <label>Nickname</label>
                 <input onChange={(e) => setNickname(e.target.value)}/>
                 <br/>
+                
                 <button onClick={() => registerHandler()}>가입</button>
+                <button onClick={() => loginHandler()}>로그인</button>
+                <br/>status : {status}
+                <br/><button onClick={() => testfunc()}>test</button>
+                <br/>text : {text}
             </div>
         </div>
     )
