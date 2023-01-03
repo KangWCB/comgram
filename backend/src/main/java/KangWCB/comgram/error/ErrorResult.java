b80f6e5c063d527f3456b412d9a2330994b21091
coodori <daoh98@naver.com>
2023-01-03 15:38:08 +0900

    erorr적용시작

---
+++ backend/src/main/java/KangWCB/comgram/error/ErrorResult.java
@@ -0,0 +1,11 @@
+package KangWCB.comgram.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
