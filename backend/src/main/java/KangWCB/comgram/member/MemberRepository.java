e81b11d2b83fa614c0fa6404491b9b1abf062522
coodori <daoh98@naver.com>
2023-01-04 22:39:52 +0900

    OAuth2.0 KaKao login + jwt

--- backend/src/main/java/KangWCB/comgram/member/MemberRepository.java
+++ backend/src/main/java/KangWCB/comgram/member/MemberRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
