52caf8713862df7b291a71967140f9163303d486
mohag123 <cksgus5336@gmail.com>
2023-01-06 03:41:11 +0900

    로그인 이외 페이지만 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
     <div className="App">
 
-      <Navbar>
-      </Navbar>
+      <Navbar />
       <div>
       <Routes>
         <Route path="/" element={<Mainpage/>} />
