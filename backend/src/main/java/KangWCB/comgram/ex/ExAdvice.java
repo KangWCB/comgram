5fff88e9e532b672cc3979d5b4845956aa8f835d
coodori <daoh98@naver.com>
2023-01-09 19:58:09 +0900

    member error msg

--- backend/src/main/java/KangWCB/comgram/ex/ExAdvice.java
+++ backend/src/main/java/KangWCB/comgram/ex/ExAdvice.java
@@ -17,8 +17,11 @@ import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
-public class ExcAdvice {
+public class ExAdvice {
 
+    /**
+     * 등록폼 error 처리
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MemberRegisterEx.class)
     public ErrorResult memberRegisterEx(MemberRegisterEx e){
@@ -26,6 +29,9 @@ public class ExcAdvice {
         return new ErrorResult("BAD",e.getMessage());
     }
 
+    /**
+     * @Vaild bindingResult 로 처리
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex){
         Map<String, String> errors = new HashMap<>();
@@ -35,6 +41,9 @@ public class ExcAdvice {
         return ResponseEntity.badRequest().body(errors);
     }
 
+    /**
+     * 로그인 에러 처리
+     */
     @ExceptionHandler(MemberLoginEx.class)
     public ResponseEntity<Map<String, String>> memberLoginEx(MemberLoginEx e){
         Map<String, String> errors = new HashMap<>();
@@ -42,4 +51,15 @@ public class ExcAdvice {
         log.error("member error: {}", errors);
         return ResponseEntity.badRequest().body(errors);
     }
+
+    /**
+     * 기본 오류 처리
+     */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandle(Exception e) {
+        log.error("[exceptionHandle] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
 }
