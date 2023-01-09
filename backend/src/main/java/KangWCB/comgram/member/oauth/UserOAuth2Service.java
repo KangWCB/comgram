dcaed990712bb104046fe386d612fc9634dad083
coodori <daoh98@naver.com>
2023-01-09 17:54:36 +0900

    kakao login password 및 오류 해결

--- backend/src/main/java/KangWCB/comgram/member/oauth/UserOAuth2Service.java
+++ backend/src/main/java/KangWCB/comgram/member/oauth/UserOAuth2Service.java
@@ -6,7 +6,11 @@ import KangWCB.comgram.member.Role;
 import KangWCB.comgram.member.dto.MemberFormDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.PasswordOAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -16,11 +20,17 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class UserOAuth2Service extends DefaultOAuth2UserService {
 
-    private final MemberRepository memberRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public UserOAuth2Service(@Lazy PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -36,9 +46,12 @@ public class UserOAuth2Service extends DefaultOAuth2UserService {
             log.info("Oauth로 로그인 한 적있는 유저");
         } else {
 //            MemberFormDto.builder().email(email).password(String.valueOf(UUID.randomUUID())).role(Role.USER).nickname(nickname);
-            Member member = Member.builder().nickname(nickname).email(email).password(String.valueOf(UUID.randomUUID())).build();
+            String uuid = UUID.randomUUID().toString().substring(0, 6); // 임시 비밀번호 하나 만들어주기 로딩시에 필요함
+            Member member = Member.builder().nickname(nickname).email(email).password(passwordEncoder.encode(uuid)).role(Role.USER).build();
             memberRepository.save(member);
         }
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),attributes,"id");
     }
+
+
 }
