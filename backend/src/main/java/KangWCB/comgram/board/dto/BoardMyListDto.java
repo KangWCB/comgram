94b1152b12ad65e920aadaf0b9d22a454cd6a7a6
coodori <daoh98@naver.com>
2023-01-31 09:58:09 +0900

    팔로잉 팔로워 팔로우 수정

---
+++ backend/src/main/java/KangWCB/comgram/board/dto/BoardMyListDto.java
@@ -0,0 +1,12 @@
+package KangWCB.comgram.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class BoardMyListDto {
+    private Long id;
+    private String photoUrl;
+}
---
+++ backend/src/main/java/KangWCB/comgram/search/SearchApiController 2.java
@@ -0,0 +1,52 @@
+package KangWCB.comgram.search;
+
+import KangWCB.comgram.board.Board;
+import KangWCB.comgram.board.repository.BoardRepositoryImpl;
+import KangWCB.comgram.member.Member;
+import KangWCB.comgram.member.repository.MemberRepositoryImpl;
+import KangWCB.comgram.photo.PhotoService;
+import KangWCB.comgram.search.dto.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
+@Slf4j
+@RequiredArgsConstructor
+public class SearchApiController {
+    private final MemberRepositoryImpl memberRepositoryCustomInmpl;
+    private final BoardRepositoryImpl boardRepositoryImpl;
+    /**
+     * 찾아주기
+     */
+    @GetMapping
+    public SearchResponseDto find(@RequestBody SearchRequestDto requestDto){
+        String word = requestDto.getWord();
+        // 빈칸이라면
+        assert !word.isBlank() : "검색할 내용이 없습니다.";
+        if (word.startsWith("#")){
+            word = word.substring(1);
+            List<Board> wordContent = boardRepositoryImpl.findWordContent(word);
+            List<SearchBoardDto> result = new ArrayList<>();
+
+            wordContent.forEach(board -> {
+                String savePath = board.getPhoto().getSavedPath();
+                result.add(new SearchBoardDto(board.getId(), savePath));
+            });
+            return new SearchResponseDto("Board", wordContent.size(), result);
+        } else {
+            List<SearchMemberDto> result = new ArrayList<>();
+
+            memberRepositoryCustomInmpl.findMember(word).forEach(member -> {
+                String savePath = member.getPhoto().getSavedPath();
+                result.add(new SearchMemberDto(member.getId(), member.getNickName(), savePath));
+            });
+            return new SearchResponseDto("Member", memberRepositoryCustomInmpl.findMember(word).size(), result);
+        }
+    }
+
+}
